43b0837f6dab0e8594e8a2a4c6c655f87979f54a
gbaldev <gonz4@Gonzalos-MacBook-Pro.local>
2024-09-01 17:13:12 -0300

    fix: cleanning code

--- notes-server/src/app.module.ts
+++ notes-server/src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { NotesModule } from './notes/notes.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import databaseConfig from './config/database.config';
 
@@ -17,7 +16,5 @@ import databaseConfig from './config/database.config';
     NotesModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
